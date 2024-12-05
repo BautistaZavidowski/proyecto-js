e3d4941997d1105c6eafcd5171a47058ba5c632a
Juan Bautista Zavidowski <bautista.z2003@gmail.com>
2024-12-04 21:06:31 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let nickName = prompt("Ingrese aqui su nombre");
+
+if (nickName.trim().toLowerCase == "bautista") {
+    alert("Bienvenido nuevamente Bautista")
+} else {
+    alert("Usted no tiene permitido ingresar a esta pagina");
+}
